62d4ec9f69e01d01847c43bcd80f16b194523b15
Joe McCormick <joe@keybittech.com>
2025-03-31 02:01:03 -0700

    sudo docker build deploy;

--- deploy/scripts/host/deploy.sh
+++ deploy/scripts/host/deploy.sh
@@ -2,12 +2,10 @@
 
 # make doesn't rescan dependencies after the first function so this script splits them up
 
-SUDO=sudo
-
 git pull
 make build
 make host_update
-make docker_up
+sudo make docker_up
 make host_deploy_op
 
 
